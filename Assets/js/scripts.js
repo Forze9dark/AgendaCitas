5946054dd144ed6b0c4aadd60a555f3d19d642de
Carlos Sanchez <carlos.sanchez.despero@gmail.com>
2021-06-05 15:26:28 -0400

    Reparacion de algunos errores, como que el archivo functions.php no deberia estar en la carpeta class.

--- Assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$("#btnDelete").on('click', function(){
-    swal("Good job!", "You clicked the button!", "success");
-});
-
-$("#btnNuevaCita").on('click', function(){
-    swal("Good job!", "You clicked the button!", "success");
-});
--- Class/sqlite.php
+++ Class/sqlite.php
@@ -1,5 +1,5 @@
 <?php 
-include_once("functions.php");
+include_once("../functions.php");
 
 // Class SQLITE con el nombre dbAgenda.db para la base de datos local
 class SqliteDatabase extends SQLite3 {
